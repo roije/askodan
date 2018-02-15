6525a4119a27052742698c20c9aacc02a225e06b
ROI <roijellingsgaard@gmail.com>
2018-02-15 14:42:14 +0100

    changed service api in production

--- webpack.prod.js
+++ webpack.prod.js
@@ -10,7 +10,7 @@ module.exports = merge(common, {
         }),
         new webpack.DefinePlugin({
            'process.env.NODE_ENV': JSON.stringify('production'),
-           '__API__': JSON.stringify('http://46.101.29.171:8000')
+           '__API__': JSON.stringify('http://46.101.29.171')
         })
     ],
 });
